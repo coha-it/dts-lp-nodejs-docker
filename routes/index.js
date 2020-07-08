3fa19d9ee2663f3ce8633b01b58db26a528e0fe2
abachschmid <a.bachschmid@corporate-happiness.de>
2020-07-08 22:37:56 +0200

    unnecessary js removed

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Dreamteam Survey' });
+  res.render('home', {
+    title: 'Dreamteam Survey',
+    jsFiles: require('../plugins/js-files'),
+    cssFiles: require('../plugins/css-files')
+  });
 });
 
 module.exports = router;
