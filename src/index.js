48a013d532b5f137356c37f480236b4c03663d36
abachschmid <a.bachschmid@corporate-happiness.de>
2020-07-08 18:18:19 +0200

    Express JS installed

--- src/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const express = require('express');
-const port = process.env.PORT || 3000;
-const app = express();
-
-app.get('/', (req, res) => res.send('Hello World! 4'));
-
-app.listen(port, () => console.log(`App listening on port ${port}!`));
